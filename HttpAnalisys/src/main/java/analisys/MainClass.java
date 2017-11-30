862fee34500d31dd1a281c6b5e6da9b1110d4cce
Luigi Durso <alduin.93y@gmail.com>
2017-11-30 15:51:02 +0100

    refactor main class

--- HttpAnalisys/src/main/java/analisys/MainClass.java
+++ HttpAnalisys/src/main/java/analisys/MainClass.java
@@ -7,7 +7,6 @@ import utility.Facebook;
 import utility.GoogleSearch;
 import utility.Instagram;
 import utility.Patreon;
-import utility.StringDB;
 import utility.YahooSearch;
 
 
@@ -17,16 +16,22 @@ public class MainClass {
 		
 		String chromeWebDriverPath = "C:\\Users\\Durso\\Downloads\\chromedriver_win32\\chromedriver.exe";
 		String epicBrowserPath = "C:\\Users\\Durso\\AppData\\Local\\Epic Privacy Browser\\Application\\epic.exe";
+		
 		WebDriver driver = EpicWebDriver.getEpicWebDriver(chromeWebDriverPath, epicBrowserPath);
+		//ricerca su yahoo answer
 		GoogleSearch.selectFirstElementOnGoogle(driver, "yahoo answer");
 		YahooSearch.selectFirstElementOnAnswer(driver, "movimento 5 stelle");
+		Thread.sleep(3000);
+		//accesso a Facebook
 		GoogleSearch.selectFirstElementOnGoogle(driver, "Facebook");
 		Facebook.FacebookAccess(driver, "luciafasolino25121994@gmail.com", "25121994");
-		Thread.sleep(1000);
+		Thread.sleep(3000);
+		//accesso a instagram e ricerca
 		GoogleSearch.selectFirstElementOnGoogle(driver, "Instagram");
 		Instagram.InstagamAccess(driver, "luciafasolino4592", "25121994");
 		Instagram.searchPersonAndPhotoLike(driver, "vincenzofalconedelicious");
-		Thread.sleep(1000);
+		Thread.sleep(3000);
+		//ricerca su patreon e condivisione
 		GoogleSearch.selectFirstElementOnGoogle(driver, "patreon");
 		Patreon.SerachOnPatreonAndShare(driver, "Nail Art");
 	}

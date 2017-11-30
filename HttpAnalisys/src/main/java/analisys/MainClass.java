c60be4e8ec4fe8969845da19c2a8b6539b1aea39
Giovanni Leo <giovannileo595@gmail.com>
2017-11-30 15:33:23 +0100

    bug fix and patreon

--- HttpAnalisys/src/main/java/analisys/MainClass.java
+++ HttpAnalisys/src/main/java/analisys/MainClass.java
@@ -6,6 +6,7 @@ import utility.EpicWebDriver;
 import utility.Facebook;
 import utility.GoogleSearch;
 import utility.Instagram;
+import utility.Patreon;
 import utility.StringDB;
 
 
@@ -16,12 +17,17 @@ public class MainClass {
 		String chromeWebDriverPath = "C:\\Users\\johnn\\Librerie\\chromedriver.exe";
 		String epicBrowserPath = "C:\\Users\\johnn\\AppData\\Local\\Epic Privacy Browser\\Application\\epic.exe";
 		WebDriver driver = EpicWebDriver.getEpicWebDriver(chromeWebDriverPath, epicBrowserPath);
-		GoogleSearch.selectFirstElementOnGoogle(driver, "Instagram");
-//		Facebook.FacebookAccess(driver, "luciafasolino25121994@gmail.com", "25121994");
+		GoogleSearch.selectFirstElementOnGoogle(driver, "Facebook");
+		Facebook.FacebookAccess(driver, "luciafasolino25121994@gmail.com", "25121994");
 //		Facebook.publishState(driver, StringDB.facebookStatus);
+		Thread.sleep(1000);
+		GoogleSearch.selectFirstElementOnGoogle(driver, "Instagram");
 		Instagram.InstagamAccess(driver, "luciafasolino4592", "25121994");
 		Instagram.searchPersonAndPhotoLike(driver, "vincenzofalconedelicious");
-		GoogleSearch.selectFirstElementOnGoogle(driver, "Facebook");
+		Thread.sleep(1000);
+		GoogleSearch.selectFirstElementOnGoogle(driver, "patreon");
+		Patreon.SerachOnPatreonAndShare(driver, "Nail Art");
+		
 		
 
 	}
---
+++ HttpAnalisys/src/main/java/utility/Patreon.java
@@ -0,0 +1,45 @@
+package utility;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Patreon {
+
+	public static void SerachOnPatreonAndShare(WebDriver driver,  String keyWord) throws InterruptedException
+	{
+		String firstElementXpath = "//*[@id='renderPageContentWrapper']/div/div/div/div[2]/div[1]/div/div[1]/a";
+		String searchQuery = "https://www.patreon.com/search?q="+keyWord;
+		String facebookShareXpath = "//*[@id='renderPageContentWrapper']/div/div[3]/div/div/div/div[3]/div/div/div/div/div[1]/button/div/div";
+		String popupShareXpath = "//*[@id='u_0_1x']";
+		Thread.sleep(1000);
+//		WebElement serarchBox = driver.findElement(By.xpath(searchBoxXpath));
+//		serarchBox.sendKeys(keyWord);
+		driver.get(searchQuery);
+		Thread.sleep(2000);
+		WebElement searchResult = driver.findElement(By.xpath(firstElementXpath));
+		searchResult.click();
+		Thread.sleep(2000);
+		WebElement facebookShare = driver.findElement(By.xpath(facebookShareXpath));
+		facebookShare.click();
+		Thread.sleep(2000);
+		String parentWindowHandler = driver.getWindowHandle(); // Store your parent window
+		String subWindowHandler = null;
+
+		Set<String> handles = driver.getWindowHandles(); // get all window handles
+		Iterator<String> iterator = handles.iterator();
+		while (iterator.hasNext()){
+		    subWindowHandler = iterator.next();
+		}
+		driver.switchTo().window(subWindowHandler); // switch to popup window
+		// Now you are in the popup window, perform necessary actions here
+		driver.findElement(By.xpath(popupShareXpath));
+		Thread.sleep(2000);
+		driver.switchTo().window(parentWindowHandler);  // switch back to parent window
+		
+		
+	}
+}
